fa6fbf9250d56e6f39c2e868d028dbb41c233d54
Olly <dninosores@gmail.com>
2020-07-05 20:52:04 -0700

    Added modifier for sin^2 function

---
+++ SineSquareModifier.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace dninosores.UnityAnimationModifiers
+{
+	class SineSquareModifier : PeriodicModifier
+	{
+		protected override float GetRawModifiedValue()
+		{
+			return amplitude * 2 * (Mathf.Pow(Mathf.Sin(Time.realtimeSinceStartup * frequency * Mathf.PI * 2 + phaseShift), 2) - 0.5f) + verticalShift;
+		}
+	}
+}
