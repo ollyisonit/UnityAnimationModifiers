094546a8b11ab6522ce3d8e9ea7e34a37bc5f54f
Olly <dninosores@gmail.com>
2020-07-06 20:31:42 -0700

    Formatting changes

--- CustomCurveModifier.cs
+++ CustomCurveModifier.cs
@@ -4,7 +4,8 @@ namespace dninosores.UnityAnimationModifiers
 {
 	class CustomCurveModifier : PeriodicModifier
 	{
-		public AnimationCurve curve;
+		public AnimationCurve curve = new AnimationCurve();
+
 		protected override float GetRawModifiedValue()
 		{
 			return amplitude * curve.Evaluate(Time.realtimeSinceStartup * frequency + phaseShift / (Mathf.PI * 2)) + verticalShift;
--- LateUpdateFloatModifier.cs
+++ LateUpdateFloatModifier.cs
@@ -1,5 +1,5 @@
 ﻿using dninosores.UnityAnimationModifiers.ValueAccessors;
-using dninosores.UnityConditionalHideAttribute;
+using dninosores.UnityEditorAttributes;
 using dninosores.UnityValueAccessors;
 using System;
 using UnityEngine;
@@ -20,221 +20,70 @@ namespace dninosores.UnityAnimationModifiers
 			Reflected
 		}
 
-		[Header("Accessor"), Space(10)]
-		public AccessType accessType;
+		//#region Modifier_Modifiers
+		//public enum ModifierType
+		//{
+		//	GeneralFloat,
+		//	Periodic,
+		//	Blend
+		//}
 
-		[ConditionalHide("accessType", AccessType.Transform)]
-		public TransformFloatValueAccessor transformToModify;
+		//[ConditionalHide("accessType", AccessType.Modifier)]
+		//public ModifierType modifierType;
 
-		#region LightModifiers
-		public enum LightAttribute
-		{
-			General,
-			Color
-		}
+		//[ConditionalHide(new string[] { "accessType", "modifierType" }, new object[] { AccessType.Modifier, ModifierType.GeneralFloat })]
+		//public ModifierFloatValueAccessor modifierToModify;
 
-		[ConditionalHide(new string[] { "accessType" }, new object[] { AccessType.Light })]
-		public LightAttribute lightAttribute;
+		//[ConditionalHide(new string[] { "accessType", "modifierType" }, new object[] { AccessType.Modifier, ModifierType.Periodic })]
+		//public PeriodicModifierFloatValueAccessor periodicModifierToModify;
 
-		[ConditionalHide(new string[] { "accessType", "lightAttribute" }, new object[] { AccessType.Light, LightAttribute.General })]
-		public LightFloatValueAccessor lightToModify;
+		//[ConditionalHide(new string[] { "accessType", "modifierType" }, new object[] { AccessType.Modifier, ModifierType.Blend })]
+		//public BlendModifierFloatValueAccessor blendToModify;
 
-		[ConditionalHide(new string[] { "accessType", "lightAttribute" }, new object[] { AccessType.Light, LightAttribute.Color })]
-		public LightColorFloatValueAccessor lightColorToModify;
+		//#endregion
 
-		#endregion
-
-		[ConditionalHide("accessType", AccessType.RectTransform)]
-		public RectTransformFloatValueAccessor rectToModify;
-
-		[ConditionalHide("accessType", AccessType.Custom), Tooltip("Make a script that extends CustomFloatValueAccessor and reference it here")]
-		public CustomFloatValueAccessor customAccessor;
-
-		[ConditionalHide("accessType", AccessType.ImageColor)]
-		public ImageColorFloatValueAccessor imageToModify;
-
-
-		#region Modifier_Modifiers
-		public enum ModifierType
-		{
-			GeneralFloat,
-			Periodic,
-			Blend
-		}
-
-		[ConditionalHide("accessType", AccessType.Modifier)]
-		public ModifierType modifierType;
-
-		[ConditionalHide(new string[] { "accessType", "modifierType" }, new object[] { AccessType.Modifier, ModifierType.GeneralFloat })]
-		public ModifierFloatValueAccessor modifierToModify;
-
-		[ConditionalHide(new string[] { "accessType", "modifierType" }, new object[] { AccessType.Modifier, ModifierType.Periodic })]
-		public PeriodicModifierFloatValueAccessor periodicModifierToModify;
-
-		[ConditionalHide(new string[] { "accessType", "modifierType" }, new object[] { AccessType.Modifier, ModifierType.Blend })]
-		public BlendModifierFloatValueAccessor blendToModify;
-
-		#endregion
-
-		#region ReflectedModifiers
-
-		public enum ReflectionType
-		{
-			Simple,
-			Nested
-		}
-
-		[ConditionalHide("accessType", AccessType.Reflected)]
-		public ReflectionType reflectionType;
-
-		[ConditionalHide(new string[] { "accessType", "reflectionType" }, new object[] { AccessType.Reflected, ReflectionType.Simple })]
-		public ReflectedFloatValueAccessor reflectedAccessor;
-
-		[ConditionalHide(new string[] { "accessType", "reflectionType" }, new object[] { AccessType.Reflected, ReflectionType.Nested })]
-		public NestedReflectedFloatValueAccessor nestedReflectedAccessor;
-
-		#endregion
+		public AnyFloatValueAccessor floatAccessor;
+	
 		/// <summary>
 		/// Default to accessing components attached to the same object as this script.
 		/// </summary>
-		protected virtual void Reset()
-		{
-			transformToModify = new TransformFloatValueAccessor();
-			lightToModify = new LightFloatValueAccessor();
-			rectToModify = new RectTransformFloatValueAccessor();
-			imageToModify = new ImageColorFloatValueAccessor();
-			lightColorToModify = new LightColorFloatValueAccessor();
-			modifierToModify = new ModifierFloatValueAccessor();
-			periodicModifierToModify = new PeriodicModifierFloatValueAccessor();
-			blendToModify = new BlendModifierFloatValueAccessor(this);
-			reflectedAccessor = new ReflectedFloatValueAccessor();
-			nestedReflectedAccessor = new NestedReflectedFloatValueAccessor();
+		//protected virtual void Reset()
+		//{
+		//	transformToModify = new TransformFloatValueAccessor();
+		//	lightToModify = new LightFloatValueAccessor();
+		//	rectToModify = new RectTransformFloatValueAccessor();
+		//	imageToModify = new ImageColorFloatValueAccessor();
+		//	lightColorToModify = new LightColorFloatValueAccessor();
+		//	modifierToModify = new ModifierFloatValueAccessor();
+		//	periodicModifierToModify = new PeriodicModifierFloatValueAccessor();
+		//	blendToModify = new BlendModifierFloatValueAccessor(this);
+		//	reflectedAccessor = new ReflectedFloatValueAccessor();
+		//	nestedReflectedAccessor = new NestedReflectedFloatValueAccessor();
 
-			transformToModify.transform = transform;
-			lightToModify.light = GetComponent<Light>();
-			rectToModify.rectTransform = GetComponent<RectTransform>();
-			imageToModify.image = GetComponent<Image>();
-			lightColorToModify.light = GetComponent<Light>();
-			modifierToModify.modifier = GetComponent<LateUpdateFloatModifier>();
-			periodicModifierToModify.modifier = GetComponent<PeriodicModifier>();
-			blendToModify.modifier = GetComponent<BlendModifier>();
-		}
+		//	transformToModify.transform = transform;
+		//	lightToModify.light = GetComponent<Light>();
+		//	rectToModify.rectTransform = GetComponent<RectTransform>();
+		//	imageToModify.image = GetComponent<Image>();
+		//	lightColorToModify.light = GetComponent<Light>();
+		//	modifierToModify.modifier = GetComponent<LateUpdateFloatModifier>();
+		//	periodicModifierToModify.modifier = GetComponent<PeriodicModifier>();
+		//	blendToModify.modifier = GetComponent<BlendModifier>();
+		//}
 
 		protected virtual void OnValidate()
 		{
-			blendToModify.parent = this;
+			
+			//blendToModify.parent = this;
 		}
 
 		protected override float GetValue()
 		{
-			switch (accessType)
-			{
-				case AccessType.Transform:
-					return transformToModify.GetValue();
-				case AccessType.RectTransform:
-					return rectToModify.GetValue();
-				case AccessType.Light:
-					switch (lightAttribute)
-					{
-						case LightAttribute.General:
-							return lightToModify.GetValue();
-						case LightAttribute.Color:
-							return lightColorToModify.GetValue();
-						default:
-							throw new NotImplementedException("No case for LightAttribute " + lightAttribute);
-					}
-				case AccessType.ImageColor:
-					return imageToModify.GetValue();
-				case AccessType.Modifier:
-					switch (modifierType)
-					{
-						case ModifierType.GeneralFloat:
-							return modifierToModify.GetValue();
-						case ModifierType.Periodic:
-							return periodicModifierToModify.GetValue();
-						case ModifierType.Blend:
-							return blendToModify.GetValue();
-						default:
-							throw new NotImplementedException("No case for ModifierType " + modifierType);
-					}
-				case AccessType.Custom:
-					return customAccessor.GetValue();
-				case AccessType.Reflected:
-					switch (reflectionType)
-					{
-						case (ReflectionType.Simple):
-							return reflectedAccessor.GetValue();
-						case (ReflectionType.Nested):
-							return nestedReflectedAccessor.GetValue();
-						default:
-							throw new NotImplementedException("No case for GetValue for " + reflectionType);
-					}
-				default:
-					throw new NotImplementedException("No case for GetValue for accessType " + accessType + "!");
-			}
+			return floatAccessor.GetValue();
 		}
 
 		protected override void SetValue(float value)
 		{
-			switch (accessType)
-			{
-				case AccessType.Transform:
-					transformToModify.SetValue(value);
-					break;
-				case AccessType.RectTransform:
-					rectToModify.SetValue(value);
-					break;
-				case AccessType.Light:
-					switch (lightAttribute)
-					{
-						case LightAttribute.General:
-							lightToModify.SetValue(value);
-							break;
-						case LightAttribute.Color:
-							lightColorToModify.SetValue(value);
-							break;
-						default:
-							throw new NotImplementedException("No case found for LightAttribute " + lightAttribute);
-					}
-					break;
-				case AccessType.ImageColor:
-					imageToModify.SetValue(value);
-					break;
-				case AccessType.Modifier:
-					switch (modifierType)
-					{
-						case ModifierType.GeneralFloat:
-							modifierToModify.SetValue(value);
-							break;
-						case ModifierType.Periodic:
-							periodicModifierToModify.SetValue(value);
-							break;
-						case ModifierType.Blend:
-							blendToModify.SetValue(value);
-							break;
-						default:
-							throw new NotImplementedException("No case found for ModifierType " + modifierType);
-					}
-					break;
-				case AccessType.Custom:
-					customAccessor.SetValue(value);
-					break;
-				case AccessType.Reflected:
-					switch (reflectionType)
-					{
-						case (ReflectionType.Simple):
-							reflectedAccessor.SetValue(value);
-							break;
-						case (ReflectionType.Nested):
-							nestedReflectedAccessor.SetValue(value);
-							break;
-						default:
-							throw new NotImplementedException("No case for GetValue for " + reflectionType);
-					}
-					break;
-				default:
-					throw new NotImplementedException("No case for SetValue for accessType " + accessType + "!");
-			}
+			floatAccessor.SetValue(value);
 		}
 
 		protected abstract float GetRawModifiedValue();
